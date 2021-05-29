b809b63aa8157e975faf6bef8fa9e69f493d3111
mhtpky <enerka2207@gmail.com>
2021-05-29 10:31:16 +0300

    project

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 let rastgeleSayi = Math.floor(Math.random() * 20);
 let puan = 10;
 let max = 0;
-
 document.querySelector(".max").textContent = "🥇 En Yuksek Puan :" + max;
-
 document.querySelector(".puan").textContent = "💯 Puan :" + puan;
-document.querySelector("body").style.backgroundColor = "#2d3436";
 
 document.querySelector(".check").onclick = function() {
     const input = document.querySelector(".input").value;
@@ -14,13 +11,11 @@ document.querySelector(".check").onclick = function() {
         ilkMsj.textContent = "🎉 Tebrikler Bildiniz 👏";
         document.querySelector(".div").textContent = rastgeleSayi;
         document.querySelector("body").style.backgroundColor = "green";
-        document.querySelector(".max").textContent = "🥇 En Yuksek Puan :" + puan;
-        if (puan > max) {
-            max += puan;
-        }
-        return;
+        document.querySelector(".max").textContent = "🥇 En Yuksek Puan :" + max;
+        document.querySelector(".max").textContent =
+            "🥇 En Yuksek Puan :" + (puan + max);
     } else if (input < rastgeleSayi) {
-        ilkMsj.textContent = "📈 Arttir ";
+        ilkMsj.textContent = "📈 Arttir";
         puan--;
         document.querySelector(".puan").textContent = "💯 Puan :" + puan;
     } else {
@@ -33,4 +28,16 @@ document.querySelector(".check").onclick = function() {
         ilkMsj.textContent = " Oyunu Kaybettiniz 😔";
         document.querySelector(".div").textContent = rastgeleSayi;
     }
+};
+
+document.querySelector(".tekrar").onclick = function() {
+    document.querySelector(".check").disabled = false;
+    let rastgeleSayi = Math.floor(Math.random() * 20);
+    document.querySelector(".puan").textContent = "💯 Puan :" + puan;
+    puan = 10;
+    document.querySelector(".puan").textContent = "💯 Puan :" + puan;
+    document.querySelector("body").style.backgroundColor = "#2d3436";
+    document.querySelector(".div").textContent = "?";
+    document.querySelector(".input").value = "";
+    document.querySelector(".ilkMsj").textContent = "Tahmine Baslaniyor!!";
 };
